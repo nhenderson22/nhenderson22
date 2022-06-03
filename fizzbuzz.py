0213604b6474fdf1259772dbe94f298d8388aa87
nhenderson22 <102487597+nhenderson22@users.noreply.github.com>
2022-06-02 18:53:05 -0600

    Update fizzbuzz.py
    changed it to leetcode style answer

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,18 @@
-x = 1
-while x != 101:
-    if x % 5 ==0 and x % 3 == 0:
-        print("FizzBuzz")
-        x=x+1
-    elif x % 3 == 0:
-        print("Fizz")
-        x=x+1
-    elif x % 5 == 0:
-        print("Buzz")
-        x=x+1
-    else:
-        print(x)
-        x=x+1
+
+class Solution(n):
+    def sol(self,n):
+        ans = []
+        for num in range(1,n+1):
+            if divBy3 and divBy5:
+                ans.append("FizzBuzz")
+            elif divBy3:
+                ans.append("Fizz")
+            elif divBy5:
+                ans.append("Buzz")
+            else:
+                print(x)
+        return ans
+
+
+fb = Solution(60)
+fb.sol()
